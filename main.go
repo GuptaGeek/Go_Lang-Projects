73e57e1a482eb28a7fb9731dc67ecfd116d337f8
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-01-24 17:50:44 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("basic questions")
-	fmt.Println(palindromecheck(121))
+	// slice := deck{"0 Ace of cards", "1 Ace of cards", "2 Ace of cards"}
+	cards := newDeck()
+	/*cards1, cards2 := deal(cards, 5)
+	fmt.Println("CARDS1-----------CARDS1")
 
-}
-func palindromecheck(n int) bool {
-	initialvalue := n
-	m := 0
-	for {
-		r := n % 10
-		m = (m * 10) + r
-		n = n / 10
-		if n == 0 {
-			break
-		}
-	}
-	if initialvalue == m {
-		return true
-	}
-	return false
+	cards1.print()
+	fmt.Println("CARDS2-----------CARDS2")
+	cards2.print()
+	// slice:=[]int{12,3}
+	// fmt.Println(slice)
+	// greeting := "Hi There!"
+	// fmt.Println([]byte(greeting))*/
+	//fmt.Println(cards.toString())
+	// fmt.Println(cards.saveToFile("my_cards"))
+	// fmt.Println(newDeckFromFile("my_cards"))
+	// slice := newDeckFromFile("my_cards")
+	// slice.print()
+	cards.shuffle()
+	cards.print()
+
 }
